6d99c1f2a35f09932b308ff62bec88f98df87ce5
LazaroESV <lazarosagarra18@gmail.com>
2026-04-10 10:44:57 -0400

    Fixed issues with level completion script

--- Project BiFractal/Assets/Scripts/LevelCompletion.cs
+++ Project BiFractal/Assets/Scripts/LevelCompletion.cs
@@ -3,17 +3,18 @@ using UnityEngine.SceneManagement;
 
 public class LevelCompletion : MonoBehaviour
 {
-    [SerializeField] private string nextScene;
+    [SerializeField] 
+    private string nextScene;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            Invoke(nameof(LoadScene), 0.5f);
+            Invoke(nameof(GoToNextLevel), 0.5f);
         }
     }
 
-    void LoadScene()
+    void GoToNextLevel()
     {
         SceneManager.LoadScene(nextScene);
     }
